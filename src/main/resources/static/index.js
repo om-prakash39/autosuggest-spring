7000c1588633c0712bfa021113b2ac0df65b9ac2
Om-prakash39 <omprakashbiradar39@gmail.com>
2026-07-18 12:07:59 +0530

    togglecommit

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,2 +1,27 @@
-//alert("i am  java")
-console.log("hello from java")
+var users = [
+    {
+        "name" : "john doe",
+        "gender" : "MALE",
+        "img" : "john.png"
+    },
+    { 
+        "name" : "jane doe",
+        "gender" : "FEMALE",
+        "img" : "jane.png"
+    }
+];
+
+var id = 0;
+
+function toggleuser() {
+    id = (id + 1) % users.length; // cycle through users
+
+    var userImage = document.getElementById("user-Image");
+    userImage.src = users[id].img; // use [] not ()
+
+    var username = document.getElementById("user-name");
+    username.innerHTML = users[id].name; // lowercase 'id'
+
+    var usergender = document.getElementById("user-gender");
+    usergender.innerHTML = users[id].gender;
+}
